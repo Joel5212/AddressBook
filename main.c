1ca83142ac37a9a60496a6feb274c9b04c265651
Michael Keshishian <mkeshishian@cpp.edu>
2021-10-14 21:15:17 -0700

    main function ill be adding edit function to

--- main.c
+++ main.c
@@ -6,33 +6,15 @@ If it returns the Status e_success:
 		prompting the method save_file() from address_book_foops.c*/
 
 #include <stdio.h>
-#include <stdio_ext.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "address_book.h"
-#include "address_book_fops.h"
 #include "address_book_menu.h"
 
-int main(void)
+struct ContactInfo
 {
-	AddressBook address_book;
-	Status ret;
-
-	/* Load the file from .csv file if any */
-	ret = load_file(&address_book);
-
-	if (ret == e_success)
-	{
-		/* Show all the available menu */
-		ret = menu(&address_book);
-
-		if (ret == e_success)
-		{
-			/* Save the entries */
-			save_prompt(&address_book);
-		}
-	}
-
-	return 0;
-}
+	char name[32];
+	char phoneNumbers[32];
+	char emailAddresses[32];
+};
